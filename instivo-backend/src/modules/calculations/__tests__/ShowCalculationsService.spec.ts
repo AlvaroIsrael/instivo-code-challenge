51d9a84605d7a5eeafb21af0e93a101ab1a594f0
AlvaroIsrael <israel.nunes.leite@gmail.com>
2025-03-11 21:00:56 -0300

    test: removes database dependency

--- instivo-backend/src/modules/calculations/__tests__/ShowCalculationsService.spec.ts
+++ instivo-backend/src/modules/calculations/__tests__/ShowCalculationsService.spec.ts
@@ -1,25 +1,14 @@
 import 'reflect-metadata';
 
+import { CalculationModel } from '@entities/Calculation.entity';
 import ShowCalculationsService from '@modules/calculations/services/ShowCalculations.service';
 import CalculationsRepository from '@repositories/Calculations.repository';
 import ICalculationsRepository from '@repositories/interfaces/ICalculationsRepository';
-import { closeConnection, openConnection } from '@shared/database/dataSource';
-import logger from 'debug';
 
 let showCalculationsService: ShowCalculationsService;
 let calculationsRepository: ICalculationsRepository;
 
 describe('ShowCalculationsService', () => {
-  beforeAll(() => {
-    openConnection()
-      .then((connection) => {
-        logger.log(`Database initialized: ${connection.readyState}`);
-      })
-      .catch((err) => {
-        logger.log(`💣 Error initializing the application 💥: ${err}`);
-      });
-  });
-
   beforeEach(() => {
     calculationsRepository = new CalculationsRepository();
     showCalculationsService = new ShowCalculationsService(
@@ -31,12 +20,22 @@ describe('ShowCalculationsService', () => {
     jest.clearAllMocks();
   });
 
-  afterAll(async () => {
-    await closeConnection();
+  test('Deve retornar erro 404 se o repositório não encontrar nenhum cálculo', async () => {
+    jest.spyOn(calculationsRepository, 'findOne').mockResolvedValue(null);
+
+    try {
+      await showCalculationsService.execute({
+        id: 'que id é esse?',
+      });
+    } catch (error) {
+      // @ts-ignore
+      expect(error.message).toBe('Calculation not found');
+    }
   });
 
-  test('Deve retornar erro 404 se o repositório não encontrar nenhum cálculo', async () => {
-    await calculationsRepository.create({
+  test('Deve retornar um cálculo se o id existir no banco', async () => {
+    const foundCalculation = new CalculationModel({
+      idUuid: 'b02e87ca-0a83-4c95-ba4f-b260aa557ab2',
       daysPassed: 1,
       monthsPassed: 1,
       salaryPercentage: 350,
@@ -58,44 +57,19 @@ describe('ShowCalculationsService', () => {
       },
     });
 
-    try {
-      await showCalculationsService.execute({
-        id: 'que id é esse?',
-      });
-    } catch (error) {
-      // @ts-ignore
-      expect(error.message).toBe('Calculation not found');
-    }
-  });
+    const findOneSpy = jest
+      .spyOn(calculationsRepository, 'findOne')
+      .mockResolvedValue(foundCalculation);
 
-  test('Deve retornar um cálculo se o id existir no banco', async () => {
-    const calculation = await calculationsRepository.create({
-      daysPassed: 1,
-      monthsPassed: 1,
-      salaryPercentage: 350,
-      yearsPassed: 1,
-      zipCodeData: {
-        cep: '12345-699',
-        logradouro: 'Rua Teste',
-        complemento: 'Complemento Teste',
-        unidade: 'Unidade Teste',
-        bairro: 'Bairro Teste',
-        localidade: 'Cidade Teste',
-        uf: 'TS',
-        estado: 'São Paulo',
-        regiao: 'Sudeste',
-        ibge: '1234567',
-        gia: '1234567',
-        ddd: '11',
-        siafi: '1234',
-      },
-    });
-
-    const foundCalculation = await showCalculationsService.execute({
-      id: calculation.idUuid,
+    const calculation = await showCalculationsService.execute({
+      id: foundCalculation.idUuid,
     });
 
     expect(foundCalculation).not.toBeNull();
-    expect(foundCalculation?.idUuid).toEqual(calculation.idUuid);
+    expect(foundCalculation.idUuid).toEqual(calculation?.idUuid);
+    expect(findOneSpy).toHaveBeenCalledTimes(1);
+    expect(findOneSpy).toHaveBeenCalledWith({
+      idUuid: foundCalculation.idUuid,
+    });
   });
 });
